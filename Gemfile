34706319a78d63f4b15a9fa239fd0a7ddf35f83b
Raghav Haran <rharan5930@gmail.com>
2013-11-24 21:57:00 -0500

    Customize bootstrap variables

--- Gemfile
+++ Gemfile
@@ -9,7 +9,7 @@ gem 'coffee-rails', '~> 4.0.0'
 gem 'jquery-rails'
 gem 'turbolinks'
 gem 'jbuilder', '~> 1.2'
-gem 'bootstrap-sass', github: 'thomas-mcdonald/boostrap-sass', branch: '3'
+gem 'bootstrap-sass', github: 'thomas-mcdonald/bootstrap-sass', branch: '3'
 
 group :doc do
   gem 'sdoc', require: false
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Pinteresting::Application.routes.draw do
   root "pages#home"
   get "about" => "pages#about"
   get "contact" => "pages#contact"
+  get "browse" => "pages#browse"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
